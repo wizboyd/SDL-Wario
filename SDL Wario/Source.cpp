f3d460d765b3c45d82555f771ce0a8772eb2afdb
wizboyd <wizboyd@gamersrus.ca>
2021-06-02 04:49:11 +0100

    Loading an BMP Image

--- SDL Wario/Source.cpp
+++ SDL Wario/Source.cpp
@@ -4,36 +4,84 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[]) {
-	SDL_Window* window = NULL;
+SDL_Window* gwindow = NULL;//NA
 
-	SDL_Surface* screensurf = NULL;
+SDL_Surface* gscreensurface = NULL;//NA
 
+SDL_Surface* Mario = NULL;
+
+bool init() {
+	//INIT FLag
+	bool success = true;
+
+	//INIT SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)//check out what a subsystem is??
 	{
 		printf("SDL COULD NOT BE INITIALIZED! SDL_ERROR: %s\n", SDL_GetError());
+		success = false;
 	}
 	else
 	{
-		window = SDL_CreateWindow("WARIO Primark", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		gwindow = SDL_CreateWindow("WARIO Primark", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gwindow == NULL)
 		{
 			printf("window Could not be created! SDL_ERROR: %s\n", SDL_GetError());
 		}
 		else
 		{
-			screensurf = SDL_GetWindowSurface(window);
+			gscreensurface = SDL_GetWindowSurface(gwindow);
+		}
+	}
+	return success;
+}
 
-			SDL_FillRect(screensurf, NULL, SDL_MapRGB(screensurf->format, 0xFF, 0xFF, 0xFF));//whats -> mean
+bool load_media() {
+	bool success = true;
 
-			SDL_UpdateWindowSurface(window);//need to update after rendering...
+	Mario = SDL_LoadBMP("Mario_staffordshire/test.bmp");
+	if (Mario == NULL)
+	{
+		printf("COuld not load Image! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_FreeSurface(Mario);
+	Mario = NULL;
+
+	SDL_DestroyWindow(gwindow);
+	gwindow = NULL;
+
+	SDL_Quit();
+}
+
+
+int main(int argc, char* args[]) {
+
+	if (!init())
+	{
+		printf("Failed to Initialize!\n");
+	}
+	else
+	{
+		if (!load_media())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			SDL_BlitSurface(Mario, NULL, gscreensurface, NULL);
+
+			SDL_UpdateWindowSurface(gwindow);
 
 			SDL_Delay(2000);
 		}
 	}
-	SDL_DestroyWindow(window);
 
-	SDL_Quit();
+	close();
 
 	return 0;
 }
